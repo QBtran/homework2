9968b7c4810e78a5f13e0f07dc0a14e37c9577f0
QBtran <84630028+QBtran@users.noreply.github.com>
2021-06-26 22:40:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let modal = document.getElementById('id01');
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+let modal2 = document.getElementById('id02');
+
+
+window.onclick = function(event) {
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+}
